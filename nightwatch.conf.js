a3f2786292eaa62089665101965b49a5d7eeba85
Sam R <samripp@gmail.com-->
2019-07-16 11:51:54 -0700

    Nightwatch

---
+++ nightwatch.conf.js
@@ -0,0 +1,52 @@
+const properties = require('./nightwatch.props')
+module.exports = {
+  "src_folders": "tests",
+  "page_objects_path": "page_objects",
+  "selenium": {
+    "start_process": true,
+    "server_path": properties.resourcePath + properties.seleniumServer,
+    "log_path": "",
+    "port": 4445,
+    "cli_args": {
+      "webdriver.chrome.driver": properties.resourcePath + properties.chromedriver,
+      "webdriver.gecko.driver": properties.resourcePath + properties.geckodriver,
+    }
+  },
+  "test_settings": {
+    "test_workers" : true,
+    "default": {
+      "launch_url": "http://localhost",
+      "selenium_port": 4445,
+      "selenium_host": "localhost",
+      "silent": true,
+      "screenshots": {
+        "enabled": false,
+        "path": ""
+      },
+      "desiredCapabilities": {
+        "browserName": "chrome",
+      }
+    },
+    "gecko": {
+      "desiredCapabilities": {
+        "browserName": "firefox",
+        "marionette": true
+      }
+    },
+    "chromeMobile": {
+      "desiredCapabilities": {
+        "browserName": "chrome",
+        "chromeOptions": {
+          "args": [
+            "window-size=400,800"
+          ]
+        }
+      }
+    },
+    "safari": {
+      "desiredCapabilities": {
+        "browserName": "safari",
+      }
+    }
+  }
+}
---
+++ nightwatch.props.js
@@ -0,0 +1,5 @@
+module.exports = {
+    resourcePath: "C:\\Users\\samri\\DevMtn\\testing-resources\\",
+    seleniumServer: "selenium-server-standalone-3.141.59.jar",
+    chromedriver: "chromedriver.exe"
+ }
